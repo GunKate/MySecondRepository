1b8100783a5b6c874503b8075459980b654a62c7
GunKate <gokhfeld.ev@gmail.com>
2024-05-20 15:30:30 +0500

    begin

--- DZUseSelenium.py
+++ DZUseSelenium.py
@@ -1,47 +1,21 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
 import time
+import random
 
 
-def search_wikipedia(query):
-    # Путь до вашего chromedriver
-    driver_path = '/path/to/chromedriver'
-    driver = webdriver.Chrome(executable_path=driver_path)
 
-    try:
-        # Открываем Википедию
-        driver.get("https://www.wikipedia.org/")
-
-        # Находим поле поиска и вводим запрос
-        search_box = driver.find_element_by_id("searchInput")
-        search_box.send_keys(query)
-        search_box.send_keys(Keys.RETURN)
-
-        time.sleep(2)  # Ждем, пока страница загрузится
-
-        while True:
-            # Получаем все параграфы с текущей страницы
-            paragraphs = driver.find_elements_by_xpath("//p")
-
-            # Выводим текст параграфов
-            for para in paragraphs:
-                print(para.text)
-                print()
-
-            # Спрашиваем пользователя, хочет ли он продолжить или выйти
-            user_input = input("Введите 'c' для продолжения пролистывания параграфов, или 'q' для выхода: ")
-            if user_input.lower() == 'q':
-                break
-
-            # Прокручиваем страницу вниз
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(2)  # Ждем, пока страница загрузится
-
-    finally:
-        # Закрываем браузер
-        driver.quit()
+# Запрашиваем у пользователя ввод
+user_input1 = input("Введите ваш запрос для поиска информации в Википедии: ")
 
 
-if __name__ == "__main__":
-    query = input("Введите запрос для поиска в Википедии: ")
-    search_wikipedia(query)
+browser = webdriver.Chrome()
+browser.get("https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0")
+
+assert "Википедия" in browser.title
+time.sleep(5)
+
+search_box = browser.find_element(By.ID, "searchInput")
+search_box.send_keys(user_input1)
+search_box.send_keys(Keys.RETURN)
